c6f10a1ff931ba0d5dff3c7c006862476275afc3
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-07-10 08:32:09 -0600

    Works

--- js/controllers/EventDetailController.js
+++ js/controllers/EventDetailController.js
@@ -11,6 +11,12 @@
         if(!vm.event.Video_Trusted) vm.event.Video_Trusted = $sce.trustAsResourceUrl(vm.event.Video__c);
         vm.highlightSpeakers = highlightSpeakers;
 
+        vm.event.Shingo_Prices__r.records.sort(function(a,b){ 
+            if(a.Order__c == undefined) a.Order__c = 1000;
+            if(b.Order__c == undefined) b.Order__c = 1000;
+            return a.Order__c - b.Order__c
+        });
+
         $scope.$emit('toggle filter', false);
     }
 })();
