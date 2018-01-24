34fea4ccfa365416eec32348531c7197eee54c1b
Abraham White <abelincoln.white@gmail.com>
2018-01-24 15:57:11 -0700

    Add Disruptor image

--- js/controllers/EventDetailController.js
+++ js/controllers/EventDetailController.js
@@ -7,7 +7,6 @@
     function EventDetailController($scope, $sce, event, highlightSpeakers){
         var vm = this;
         vm.event = event;
-        console.log('Event: ', event);
         if(!vm.event.Video_Trusted) vm.event.Video_Trusted = $sce.trustAsResourceUrl(vm.event.Video__c);
         vm.highlightSpeakers = highlightSpeakers;
 
